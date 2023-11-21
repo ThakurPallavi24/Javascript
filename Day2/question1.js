8290120801f9c9d6564a40873acb42e120e30ec0
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-11-21 12:29:49 +0530

    Operators

---
+++ Day2/question1.js
@@ -0,0 +1,24 @@
+//Operators : It ia symbol to perform operations on operands.
+
+//Airthmatic Operators.
+let a = 10;
+let b = 8;
+
+console.log("a = ", a, " & ", "b = ", b);
+
+console.log("a+b =", a + b);
+console.log("a-b =", a - b);
+console.log("a*b =", a * b);
+console.log("a/b =", a / b);
+console.log("a%b =", a % b);
+
+let c = 3;
+console.log("a**c =", a ** c);
+c = ++c;
+console.log("++c =", c);
+c = --c;
+console.log("--c =", c);
+c = c++;
+console.log("c++ = ", c);
+c = c--;
+console.log("c-- =", c);
---
+++ Day2/question4.js
@@ -0,0 +1,6 @@
+//Logical Operator:perform logical operations and return a boolean value
+let a = 12;
+let b = 6;
+console.log(a > b && b < a); //Logical AND in this T+T=T otherwise false
+console.log(a == b || b == a); //Logical OR in this F+F=F otherwise true
+console.log(!(a > 40)); //Logical NOT it will make True to false or false to true.
