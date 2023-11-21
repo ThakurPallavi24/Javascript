07985caa74c39fe3ebbb70c3852118fafb218278
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-11-21 11:18:23 +0530

    variables

---
+++ Day1/question3.js
@@ -0,0 +1,31 @@
+//Variable declaration.
+
+//A variable is a container or name for a memory location to store the value.
+
+//There are 3 keywords for variable names - var ,let,const.
+
+var studentName = "Pallavi Thakur"; // It is a global scope variable.
+
+var studentName = "Chunmun"; // Varaible can be re-declared and updated.
+
+console.log(studentName);
+
+var age = 24;
+
+var age = 65;
+
+console.log(age);
+
+let gender = "male"; // It is a block scope variable.
+
+//let gender = female; // Variable can not be re-declared.
+
+gender = "female"; // variable can be updated.
+
+console.log(gender);
+
+const state = "Bihar"; // It is a block scope variable.
+
+//const state = "Odisha"; //const variable cannot be re-declared or updated.
+
+console.log(state);
