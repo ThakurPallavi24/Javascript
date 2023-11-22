c14af07ea14cd015702fdddd40c2dd9191c749bd
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-11-22 23:35:38 +0530

    Math

---
+++ Day3/question1.js
@@ -0,0 +1,19 @@
+//Math:It allows us to perform mathematical operations on the Number type.
+
+let a = Math.abs(-5); // abs returns the absolute value of a number.
+console.log(a);
+
+let b = Math.ceil(4.25); // ceil returns the smallest integer greater than or equal to a number
+console.log(b);
+
+let c = Math.floor(7.75); // floor returns the largest integer less than or equal to a number
+console.log(c);
+
+let d = Math.round(9.6); // round returns the rounded value of the number
+console.log(d);
+
+let e = Math.pow(4, 3); // pow returns the power as x to the power y.
+console.log(e);
+
+let x = Math.PI;
+console.log(x);
