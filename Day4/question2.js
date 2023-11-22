233fb6fa951cbf58cdd2aed1fe590c68bbcb0c77
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-11-23 00:17:40 +0530

    control statements

---
+++ Day4/question2.js
@@ -0,0 +1,13 @@
+//Nested if-else
+
+let b = 12;
+let message;
+if (b > 5) {
+  message = "go to school";
+} else if (b > 25) {
+  message = "Do Job";
+} else {
+  message = "You are a kid ";
+}
+
+console.log(message);
