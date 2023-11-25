9ec65fbd2a5f04952fcacb31df46b77304f12daf
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-11-25 19:35:26 +0530

    Array

---
+++ week2/Day3/qst5.js
@@ -0,0 +1,12 @@
+//Array used to store a collection of values, such as numbers, strings, objects, or even other arrays.
+let num = [1, 2, 3, 4, 5, 6];
+
+console.log(num);
+
+console.log(num[0]);
+
+console.log(num[5]);
+
+for (const n of num) {
+  console.log(n);
+}
