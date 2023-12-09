bc64194b8d43673d6069319b04a41c25b30a8716
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-12-09 17:06:54 +0530

    Arrow function

---
+++ Practice/P6.js
@@ -0,0 +1,15 @@
+//Arrow function:It is a consize and modern way to define a function.
+
+//Q1
+let add = (a, b) => {
+  return a + b;
+};
+let result = add(7, 8);
+console.log(result);
+
+//Q2
+let square = (x) => {
+  return x * x;
+};
+let area = square(6);
+console.log(area);
