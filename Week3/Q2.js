e0f42f881dd63345612535a03d0808e089941a39
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-12-11 19:48:55 +0530

    Implicit conversion

---
+++ Week3/Q2.js
@@ -0,0 +1,16 @@
+//Implicit conversion:It  automatically converts one data to another data type.
+let c = "44";
+let c2 = "12";
+let sum = c + c2;
+let sub = c - c2;
+let mlty = c * c2;
+console.log(typeof c);
+
+console.log(sum);
+console.log(typeof sum);
+
+console.log(sub);
+console.log(typeof sub);
+
+console.log(mlty);
+console.log(typeof mlty);
