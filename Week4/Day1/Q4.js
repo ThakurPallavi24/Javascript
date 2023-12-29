89c2f3ed411f6627a3e054cb3aa16bac5f09d9a4
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-12-29 21:15:51 +0530

    String length

---
+++ Week4/Day1/Q4.js
@@ -0,0 +1,6 @@
+//Write a function that takes a string as an argument and returns its length.
+function str(a) {
+  const length = a.length;
+  return length;
+}
+console.log(str("Pallavi Thakur"));
