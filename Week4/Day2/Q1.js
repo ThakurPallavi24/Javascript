e1dd92a5a41309f31f9b0eeab54f3b927ef3f61a
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-12-30 19:15:48 +0530

    function

---
+++ Week4/Day2/Q1.js
@@ -0,0 +1,8 @@
+//Complete the function shoutMyName such that it returns the name parameter it receives all in upper case.
+function shoutMyName(name) {
+  let a = name.toUpperCase();
+  return a;
+}
+
+console.log(shoutMyName("Sam"));
+console.log(shoutMyName("Charley"));
